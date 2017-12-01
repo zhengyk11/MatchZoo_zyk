6e5dcab13d2a18c41f2328f100e0a7d0f215d75e
郑玉昆 <zhengyk13@gmail.com>
2017-12-01 21:13:36 +0800

    cdssm works

--- matchzoo/models/cdssm.py
+++ matchzoo/models/cdssm.py
@@ -14,7 +14,7 @@ class CDSSM(BasicModel):
     def __init__(self, config):
         super(CDSSM, self).__init__(config)
         self.__name = 'CDSSM'
-        self.check_list = [ 'text1_maxlen', 'text2_maxlen',
+        self.check_list = [ 'query_maxlen', 'doc_maxlen',
                    'vocab_size', 'embed_size',
                    'kernel_count', 'kernel_size', 'hidden_sizes']
         self.embed_trainable = config['train_embed']
@@ -43,8 +43,8 @@ class CDSSM(BasicModel):
                     seq.add(Dense(self.config['hidden_sizes'][i+1], activation='relu'))
                 seq.add(Dense(self.config['hidden_sizes'][-1]))
             return seq
-        query = Input(name='query', shape=(self.config['text1_maxlen'],))
-        doc = Input(name='doc', shape=(self.config['text2_maxlen'],))
+        query = Input(name='query', shape=(self.config['query_maxlen'],))
+        doc = Input(name='doc', shape=(self.config['doc_maxlen'],))
 
         wordhashing = Embedding(self.config['vocab_size'], self.config['embed_size'], weights=[self.config['embed']], trainable=self.embed_trainable)
         q_embed = wordhashing(query)
@@ -52,9 +52,9 @@ class CDSSM(BasicModel):
         conv1d = Convolution1D(self.config['kernel_count'], self.config['kernel_size'], padding='same', activation='relu')
         q_conv = conv1d(q_embed)
         d_conv = conv1d(d_embed)
-        q_pool = MaxPooling1D(self.config['text1_maxlen'])(q_conv)
+        q_pool = MaxPooling1D(self.config['query_maxlen'])(q_conv)
         q_pool_re = Reshape((-1,))(q_pool)
-        d_pool = MaxPooling1D(self.config['text2_maxlen'])(d_conv)
+        d_pool = MaxPooling1D(self.config['doc_maxlen'])(d_conv)
         d_pool_re = Reshape((-1,))(d_pool)
 
 
