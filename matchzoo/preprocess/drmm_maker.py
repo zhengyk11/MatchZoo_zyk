559e08b7ec205cc577dd6a4fb40cfd70694a96b6
郑玉昆 <zhengyk13@gmail.com>
2017-12-15 21:06:54 +0800

    drmm maker

--- matchzoo/preprocess/drmm_maker.py
+++ matchzoo/preprocess/drmm_maker.py
@@ -1,7 +1,5 @@
 # suitable for drmm
 
-import multiprocessing
-import time
 import json
 import os
 import sys
@@ -10,11 +8,6 @@ sys.path.append('../')
 from utils import read_embedding, convert_term2id, convert_embed_2_numpy
 import numpy as np
 
-embed_dict = {}
-vocab_size = 0
-embed_size = 0 
-word_dict = {}
-idf_dict = {}
 
 def cal_hist(query_embed, doc_embed, config):
     hist = np.zeros([config['query_maxlen'], config['hist_size']], dtype=np.int32)
@@ -30,19 +23,18 @@ def cal_hist(query_embed, doc_embed, config):
     return hist
 
 def main():
+
     config = """
     {
         "query_maxlen": 10,
         "doc_maxlen": 1000,
         "hist_size": 200,
         "embed_path": "../../data/support/embed_query_100w_50.txt",
-        "input": "/home/luocheng/zhengyukun/bigsearch/data/bm25_top200/runtime_data_bm25",
-        "output": "/home/luocheng/zhengyukun/bigsearch/data/bm25_top200/runtime_data_bm25_drmm"
+        "input": "../../runtime_data/fulltext/real_train_HL_solrex",
+        "output": "../../runtime_data/fulltext/real_train_HL_solrex_drmm"
     }
-	"""
-    #typenum = int(sys.argv[0])
-    process_total = 30
-    cpu_num = 30
+    """
+
     config = json.loads(config)
 
     embed_dict, vocab_size, embed_size, word_dict, idf_dict = read_embedding(config['embed_path'])
@@ -51,18 +43,10 @@ def main():
 
     if not os.path.exists(config['output']):
         os.mkdir(config['output'])
-    pool = multiprocessing.Pool(processes=cpu_num)
-    for i in range(0,process_total):
-        pool.apply_async(getfile,(config,process_total,i))
-    pool.close()
-    pool.join()
 
-def getfile(config, process_total, typenum):
     for dirpath, dirnames, filenames in os.walk(config['input']):
         for fn in filenames:
             if not fn.endswith('.txt'):
-                continue
-            if (int(fn.strip().split('.')[0]) % process_total != typenum):
                 continue
             print os.path.join(dirpath, fn)
             with open(os.path.join(dirpath, fn)) as file:
